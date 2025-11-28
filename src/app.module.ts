8444a8d296e5ead45d875edc3c97f494633f0664
EuleF <vlad.krabik2@gmail.com>
2025-11-28 04:22:35 +0300

    com

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [PrismaModule, UsersModule, TasksModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
